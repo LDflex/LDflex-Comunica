LDflex/Query-Solid

Set up development environment.

test/index-test.js
@@ -1,0 +1,7 @@
+import SolidQueryLDFlex from '../src';
+
+describe('The SolidQueryLDFlex module', () => {
+  it('is an object', () => {
+    expect(SolidQueryLDFlex).toBeInstanceOf(Object);
+  });
+});

LDflex/LDflex-Comunica

test/index-test.js
@@ -1,0 +1,7 @@
+import ComunicaEngine from '../src';
+
+describe('The ComunicaEngine module', () => {
+  it('is an object', () => {
+    expect(ComunicaEngine).toBeInstanceOf(Object);
+  });
+});