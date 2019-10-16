b94f0c28c37236a1068478db9fe0622db0632f34
Joachim Van Herwegen <joachimvh@gmail.com>
2019-10-16 11:41:32 +0200

    Support all types of sources

--- src/ComunicaEngine.js
+++ src/ComunicaEngine.js
@@ -77,23 +77,29 @@ export default class ComunicaEngine {
     let sources = await source;
     if (!sources)
       return null;
+
     // Strip the fragment of a URI
     if (sources instanceof URL)
       sources = sources.href;
     else if (sources.termType === 'NamedNode')
       sources = sources.value;
+
     if (typeof sources === 'string')
       sources = [sources.replace(/#.*/, '')];
-    // Await multiple promises in an array
+    // Flatten the recursive calls to this function
     else if (Array.isArray(sources))
-      sources = await Promise.all(sources);
+      sources = [].concat(...(await Promise.all(sources.map(src => this.toComunicaSources(src)))));
+    // Needs to be after the string check since those also have a match functions
+    else if (sources.match)
+      sources = [Object.assign({ type: 'rdfjsSource' }, sources)];
     // Wrap a single source in an array
     else
       sources = [sources];
+
     // Add Comunica source details
-    return sources.map(value => ({
-      value,
-      type: typeof value === 'string' ? 'file' : 'rdfjsSource',
+    return sources.map(src => ({
+      value: src.value || src,
+      type: src.type,
     }));
   }
 }
